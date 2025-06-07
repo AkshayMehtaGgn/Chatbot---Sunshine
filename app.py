c298fa9c5b22d249a90ae8e1d8826df55f63b86a
AkshayMehtaGgn <138118314+AkshayMehtaGgn@users.noreply.github.com>
2025-06-07 14:57:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -47,13 +47,15 @@ def get_bot_answer():
 
     # Log the user info and question to a file
     try:
-        requests.post(GOOGLE_SHEET_WEBHOOK, json={
+         response = requests.post(GOOGLE_SHEET_WEBHOOK, json={
             'name': name,
             'contact': contact,
             'email': email,
             'question': question,
             'answer': answer
         })
+        print("🟢 Posted to Google Sheet:", response.status_code)
+        print("📄 Response:", response.text)
     except Exception as e:
         print("Logging to Google Sheets failed:", e)
 
